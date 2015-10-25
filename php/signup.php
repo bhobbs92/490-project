e43491e1d56faae499f011b37fd50ee78736dc93
d_rad <drg23@njit.edu>
2015-10-25 19:42:09 -0400

    fixed login/ signup (backend)

--- php/signup.php
+++ php/signup.php
@@ -3,23 +3,22 @@
 include "account.php";
 
 $userName = $_GET["username"];
-$fullName = $_GET["fullname"];
+$fullName = $_GET["fullName"];
 $address = $_GET["address"];
 $pwd = $_GET["password"];
 
-print"<h1>" . $_GET . "</h1>";
 //connect to db
 ($dbh = mysql_connect($hostDB, $userDB, $passDB)) or die("Failed to connect to DB");
 print "connected";
 mysql_select_db($userDB);
 
 //create insert queries
-$insertCustomer = "INSERT INTO `rr354`.`Customer`(`customerId`, `name`, `address`) VALUES ('$userName','$fullName', '$address')";
-$insertPassword = "INSERT INTO `rr354`.`Password` (`customerId`, `hash`) VALUES ('$userName', '$pwd');"
+$insertCustomer = "INSERT INTO `rr354`.`Customer`(`customerId`, `name`, `address`) VALUES ('$userName','$fullName', '$address');";
+$insertPassword = "INSERT INTO `rr354`.`Password` (`customerId`, `hash`) VALUES ('$userName', '$pwd')";
 
 //populate customer table
 $response = mysql_query($insertCustomer);
-($response) or die("error inserting customer");
+($response) or die(mysql_error());
 print "insert customer successful";
 
 //populate password table
