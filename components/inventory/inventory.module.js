ecd514c5cb3497201b2db040fdf37b06a97699cb
yazman12 <yazman12@gmail.com>
2015-11-01 22:21:26 -0500

    Added contextual classes for item stock amounts

--- components/inventory/inventory.module.js
+++ components/inventory/inventory.module.js
@@ -6,6 +6,11 @@
 	inventoryCtrl.$inject = ['$scope', '$http'];
 
 	function inventoryCtrl ($scope, $http) {
+		$scope.cart = [];
+
+		$scope.addToCart = addToCart;
+		$scope.getStock = getStock;
+
 		$http.get('api/inventory.php')
 			.then(function (res) {
 				var data = res.data;
@@ -14,5 +19,24 @@
 					$scope.items = data.items;
 				}
 			});
+
+		function addToCart (itemIndex) {
+			var currentItem = $scope.items[itemIndex];
+			$scope.cart.push(currentItem);
+			console.log($scope.cart);
+			currentItem.stock--;
+		}
+
+		function getStock (itemIndex) {
+			var currentItem = $scope.items[itemIndex];
+			if (currentItem.stock > 30) {
+				return 'plenty';
+			} else if (currentItem.stock > 10) {
+				return 'some';
+			} else {
+				return 'almostGone';
+			}
+
+		}
 	}
 }());
