2425b5e2127f8e2e2392462f229b4b03109bd734
d_rad <drg23@njit.edu>
2015-12-04 00:40:16 -0500

    weird parts

--- components/admin/package/package-control.js
+++ components/admin/package/package-control.js
@@ -8,6 +8,7 @@
 	function packageControl ($scope, $http, $state) {
 		$scope.packages = [];
 		$scope.toShip = [];
+		$scope.perishable = false;
 
 		$scope.addToCart = addToCart;
 
@@ -31,5 +32,63 @@
 				$scope.toShip.splice(whereItsAt, 1);
 			}
 		}
+		function getCollectiveWeight(){
+			var collectiveWeight = 0;
+			for(var package in $scope.toShip){
+					collectiveWeight += parseInt($scope.toShip[package].weight);
+
+			}
+				return collectiveWeight;
+		}
+
+		$scope.addCargo = function(){
+
+							getCargoIds = "SELECT * FROM CARGO";
+							$http.post('api/admin/databasePlease.php', getCargoIds).then(
+								function (res) {
+									var allIds= res.data[0];
+									$scope.nextCargoId = 1;
+									for(var id in allIds){
+										if(allIds[id].Skid_ID_Number > $scope.nextCargoId){
+											$scope.nextCargoId = parseInt(allIds[id].Skid_ID_Number);
+										}
+									}
+									$scope.nextCargoId++
+									console.log($scope.nextCargoId);
+									var weight = getCollectiveWeight();
+									var insertCargo = "INSERT INTO `CARGO`(`Skid_ID_Number`, `Skid_Weight`, `Skid_Contents`) VALUES ("+	$scope.nextCargoId+","+weight+", '"+(($scope.perishable)?"PERISHABLE":"NON_PERISHABLE")+"')";
+									console.log(insertCargo);
+
+
+									//insert into cargo table (works)
+									$http.post('api/admin/databasePlease.php', insertCargo).then(
+										function (res) {
+											console.log(res);
+
+
+
+											//Insert into cargo elements (villian)
+											for(var package in $scope.toShip){
+												var insertCargoEle = "INSERT INTO `Cargo_Elements`(`Skid_ID_Number`, `packageId`, `name`, `address`, `weight`)"
+																							+ "VALUES ("+$scope.nextCargoId+","+ $scope.toShip[package].packageId+",'"+ $scope.toShip[package].recipient+"','"+ $scope.toShip[package].address+"',"+ $scope.toShip[package].weight+")";
+												console.log(insertCargoEle);
+												$http.post('api/admin/databasePlease.php', insertCargo).then(
+													function (res) {
+														console.log(res);
+													});
+
+											}
+
+
+
+
+										});
+
+
+							});
+
+
+
+						}
 	}
 }());
